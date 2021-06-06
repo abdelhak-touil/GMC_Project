371135182417a495626ccdd822712cc802dea497
abdelhak-touil <abdelhak.touil1994@gmail.com>
2021-06-07 01:09:24 +0200

    authentification_backend

--- server.js
+++ server.js
@@ -7,15 +7,19 @@ const connectDB = require("./config/db");
 const notFound = require("./middleware/errorMiddleware");
 const errorHandler = require("./middleware/errorMiddleware");
 const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 connectDB();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
